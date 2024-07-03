5d651405af71da8e1f46af74e2e29bc9a53e08cd
Nikita Semenenko <Nik21081995@yandex.ru>
2024-07-03 20:08:52 +0300

    initial commit

---
+++ src/test/java/ru/netology/test/ReplanDeliveryCardFormTest.java
@@ -0,0 +1,65 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class ReplanDeliveryCardFormTest {
+
+    @BeforeAll
+    static void setUpAll(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll(){
+        SelenideLogger.removeListener("allure");
+    }
+
+
+    static void inputForm(String city, String date, String name, String phone) {
+        open("http://localhost:9999");
+
+        $("[data-test-id='city']").$("[class='input__control']").setValue(city);
+
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(date);
+        $("[data-test-id='name']").$("[class='input__control']").setValue(name);
+        $("[data-test-id='phone']").$("[class='input__control']").setValue(phone);
+        $("[data-test-id='agreement']").click();
+    }
+    @Test
+    void shouldPreventSendRequestMultipleTimes() {
+        RegistrationByCardInfo regData = DataGenerator.Registration.generate();
+        inputForm(regData.getCity(), regData.getDate(), regData.getName(), regData.getPhone());
+
+        $(".button").click();
+        $("[data-test-id='success-notification']")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + regData.getDate()), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(regData.getDateNext());
+        $(".button").click();
+
+        $("[data-test-id='replan-notification']")
+                .shouldHave(Condition.text("Перепланировать"), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+        $("[data-test-id='replan-notification'] button").click();
+
+        $("[data-test-id='success-notification']")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + regData.getDateNext()), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+    }
+
+
+
+}
